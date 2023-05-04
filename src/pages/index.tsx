f97afba0a238da8cf3926d0b9a76c02a7eb312aa
Edward Do <edwardtdo@gmail.com>
2023-05-04 16:57:31 -0600

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,146 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello, world!
+      </h1>
+      <h1 className="bg-green-600 text-white border-4 border-green-600">
+        Hello, world!
+      </h1>
+      <h1 className="border-4 border-vuejs-932">
+        Hello, world!
+      </h1>
+      <h1 className="text-yellow-600">
+        Hello, world!
+      </h1>
+      <p className="text-xs sm:text-base md:text-lg lg:text-3xl mb-1">
+        Hello long sentence
+      </p>
+      <p className="mt-[1rem] bg-[#bad455]">
+        Another long sentence
+      </p>
+
+      <h1 className="text-3xl">
+        Title 1
+      </h1>
+      <h2 className="text-2xl">
+        Title 2
+      </h2>
+      <h3 className="text-xl">
+        Title 3
+      </h3>
+      <p className="text-base leading-normal underline decoration-red-500 decoration-wavy decoration-1 underline-offset-4 uppercase">
+        A regular paragraph
+      </p>
+      <p className="text-sm">
+        A description paragraph
+      </p>
+      <p className="text-xs note">
+        A little note
+      </p>
+
+      <div>
+        <p className="truncate">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus maxime consectetur architecto similique eaque soluta, enim facere modi iste expedita ipsa. Rerum eligendi molestiae fugiat cupiditate fuga est itaque commodi.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <p className="break-all">
+          Lorem ipsum dolor, pneumonoultramicroscopicsilicovolcanoconiosis sit amet consectetur adipisicing elit. Quo, accusantium debitis. Rerum aliquid, eligendi adipisci hic laborum fuga esse placeat. Porro rerum fugiat reprehenderit debitis iusto ipsam nobis. Inventore, unde?
+        </p>
+      </div>
+
+      <div className="bg-blue-500 my-4">
+        box with margin
+      </div>
+      <div className="bg-red-500 px-4">
+        box with padding
+      </div>
+
+      <h1 className="font-bold text-2xl mb-2">
+        This is my title
+      </h1>
+      <p>
+        This is my paragraph
+      </p>
+
+      <div className="bg-blue-500 h-16 w-36 opacity-20" />
+      <div className="bg-red-300 -mt-8 mb-2">
+        -mt-8
+      </div>
+
+      <div className="flex min-h-screen space-x-4">
+        <div className="w-1/5 bg-red-500">
+          Sidebar
+        </div>
+        <div className="w-4/5 bg-green-500">
+          Main content
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <h1 className="mb-4">
+        First title
+      </h1>
+      <h2 className="mb-3">
+        Second title
+      </h2>
+      <h3 className="mb-2">
+        Third title
+      </h3>
+      <p className="mb-1">
+        paragraph
+      </p>
+
+      <div className="Parent">
+        <div>Header</div>
+        <div className="flex flex-row">
+          <div className="grow">Main content</div>
+          <div className="flex flex-col">
+            <div>Sidebar</div>
+            <div>Menu</div>
+          </div>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="Parent">
+        <div>Header</div>
+        <div className="flex">
+          <div className="basis-4/12">Sidebar</div>
+          <div className="basis-6/12">Main content</div>
+          <div className="basis-2/12">Another Sidebar</div>
+        </div>
       </div>
-    </main>
+
+      <div className="Parent">
+        <div>Header</div>
+        <div className="flex flex-col md:flex-row">
+          <div className="md:w-32 bg-sky-500">Sidebar</div>
+          <div className="w-full bg-red-600">Main content</div>
+        </div>
+      </div>
+
+      <div className="Parent">
+        <div>Header</div>
+        <div className="flex flex-wrap w-full">
+          <div className="w-32 grow">01</div>
+          <div className="w-32 grow">02</div>
+          <div className="w-32 grow">03</div>
+          <div className="w-32 grow">04</div>
+          <div className="w-32 grow">05</div>
+        </div>
+      </div>
+
+      <div className="Parent">
+        <div>Header</div>
+        <div className="flex items-center justify-evenly">
+          <div>Sidebar</div>
+          <div>
+            <h1>Main content</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere unde commodi accusantium atque delectus placeat reiciendis eius eaque provident, fugit asperiores omnis deleniti, veniam porro explicabo? Repudiandae facilis modi aperiam.
+            </p>
+          </div>
+          <div>Another sidebar</div>
+        </div>
+      </div>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,38 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {
+      margin: {
+        1: '14px',
+        2: '16px',
+        3: '18px',
+        4: '20px',
+      },
+      spacing: {
+        '1': '8px',
+        '2': '12px',
+        '3': '16px',
+        '4': '24px',
+        '5': '32px',
+        '6': '48px',
+      },
+      fontSize: {
+        xs: '12px',
+        sm: '15px',
+        base: '18px',
+      },
+      colors: {
+        vuejs: {
+          100: '#49e659',
+          932: '#434252',
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
